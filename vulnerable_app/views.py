22c0f6784ea5db13c50e1f1fe8da1edfd258f420
csyr <cpmonyok@syr.edu>
2021-11-16 18:20:58 -0700

    final version of view with vulnerable view and secure view

--- vulnerable_app/views.py
+++ vulnerable_app/views.py
@@ -6,23 +6,20 @@ def searchparts(request):
     return render(request, "partsearch.html")
 
 # SQL Injection Vulnerable
+
+
 def parts(request):
-    #searchtext = "%" + request.POST['searchText'] + "%"
-    #sql = "Select * FROM part p where p.name like %s"
     searchtext = request.POST['searchText']
     sql = ("""select * from part p where p.name='%s';""" %(searchtext))
     dsparts = Part.objects.raw(sql)
-    #dsparts = Part.objects.raw(sql, [searchtext])
-    #dsparts = Part.objects.all()
     return render(request, "partsearch.html", {'data': dsparts})
+
 
 # Using ORM Model for Security
 
 
-"""
-def parts(request):
-    searchtext = request.POST['searchText']
-    #searchtext = '%Prop%'
-    dsparts = Part.objects.all()
-    return render(request, "partsearch.html", {'data': dsparts})
-    """
+#def parts(request):
+    #searchtext = request.POST['searchText']
+    #dsparts = Part.objects.all().filter(name=searchtext)
+    #return render(request, "partsearch.html", {'data': dsparts})
+
